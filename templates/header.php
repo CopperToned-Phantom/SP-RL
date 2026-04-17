25b30022957705a04d13c1e6f06eeeaffd18365b
CopperToned Phantom <mastatime48@gmail.com>
2026-04-17 14:45:05 +0000

    test changes

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,8 @@
 <header class="header">
     <aside class="header-color" >
-        <img src="./media/LogoSPIRL.png" class="header-color">
-    </aside>
-    <aside class="header-color" >
         <h1 class="header-color" style="text-align : right"> SP/RL Init.. </h1>
     </aside>
-
+    <aside class="header-color" >
+        <img src="./media/LogoSPIRL.png" width="150px" class="header-color">
+    </aside>
 </header>
